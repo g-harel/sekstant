9939b84b1a9e23a2beb33080583cbfebd16ced73
g-harel <gabrielj.harel@gmail.com>
2019-02-20 19:20:08 -0500

    configure trusted CAs globally

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
-const fs = require("fs");
 const path = require("path");
 const os = require("os");
 
-const express = require("express");
-const graphqlHTTP = require("express-graphql");
-const got = require("got");
 const {createGraphQlSchema} = require("oasgraph");
+const express = require("express");
+const got = require("got");
+const graphqlHTTP = require("express-graphql");
+const syswidecas = require('syswide-cas');
 
 const APIServerHost = "192.168.99.100";
 const APIServerPort = 8443;
-const certPath = path.join(os.homedir(), ".minikube");
+
+syswidecas.addCAs(path.join(os.homedir(), ".minikube", "ca.crt"))
 
 const main = async () => {
     const oas = await got({
         hostname: APIServerHost,
         port: APIServerPort,
         path: "/swagger.json",
-
-        ca: fs.readFileSync(path.join(certPath, "ca.crt")),
-        cert: fs.readFileSync(path.join(certPath, "client.crt")),
-        key: fs.readFileSync(path.join(certPath, "client.key")),
-
-        timeout: 1000,
+        timeout: 3000,
         json: true,
     }).then((res) => res.body);
 
-    const {schema} = await createGraphQlSchema(oas);
+    const {schema} = await createGraphQlSchema(oas, {
+        baseUrl: `https://${APIServerHost}:${APIServerPort}`
+    });
 
     const app = express();
     app.use("/graphql", graphqlHTTP({schema, graphiql: true}));
-    app.listen(3000, () => console.log("https://localhost:3000/graphql"));
+    app.listen(3000, () => console.log("http://localhost:3000/graphql"));
 };
 
 main().catch((err) => {
